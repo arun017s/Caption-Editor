88ed586d64659872dc2b18d3b8c8954d65805992
Arun <86429222+Arun-TG@users.noreply.github.com>
2022-01-08 05:55:18 +0530

    .

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def get_caption(name):
     return newcap
 
 
-bot.on_message(filters.channel & filters.document) #add more filters if you want.
+@bot.on_message(filters.channel & filters.document) #add more filters if you want.
 async def caption(bot, message):
     await message.edit(CAPTION.format(name=get_caption(message.document.file_name),    
                                       size=get_size(message.document.file_size))
