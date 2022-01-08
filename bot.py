551c6f068ca49169a2a102e5f3cdf19f02726390
Arun <86429222+Arun-TG@users.noreply.github.com>
2022-01-08 11:27:08 +0530

    KB, .

--- bot.py
+++ bot.py
@@ -13,22 +13,23 @@ bot = Client(session_name="caption-editor",
              api_hash=os.environ.get("API_HASH"),
              bot_token=os.environ.get("BOT_TOKEN"))
 
-# experiment
+#experiment #fail
 async def get_size(size):
-    p=size/1048597
-    if p>1000:
-         c=round(p/1000, 2)
+    p=size/1048597           #size in MB
+    if p>1024:
+         c=round(p/1024, 2)
          filezize=f"{c} GB"
-    else:
+    elif p>1:
          c=round(p, 2)
          filezize=f"{c} MB"
+    else:
+         c=round(size/1024, 1)
+         filezize=f"{c} KB"
     return filezize
 
 async def get_caption(name):
     if "_" in name:
-        newcap=name.replace("_", " ") 
-    elif "." in name:
-        newcap=name.replace(".", " ")
+        newcap=name.replace("_", " ")     
     else:
         newcap=name
     return newcap
